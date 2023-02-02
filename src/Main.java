78fab915532eebd30f38778956e8c293f1725995
Михаил <m-toka@mail.ru>
2023-02-02 20:27:51 +0300

    Homework is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,104 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+
+    }
+    public static void task1() {
+        System.out.println("Задание 1");
+        int clientOS=0;
+        if (clientOS==0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS==1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Система не определена");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задание 2");
+        int clientOS=0;
+        int clientDeviceYear=2015;
+        if (clientOS==0) {
+           if (clientDeviceYear>=2015) {
+               System.out.println("Установите версию приложения для iOS по ссылке");
+           } else {
+               System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+           }
+        } else if (clientOS==1) {
+            if (clientDeviceYear>=2015) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("становите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Система не определена");
+        }
+    }
+    public static void task3() {
+        System.out.println("Задание 3");
+        int year=2021;
+        int year4=year%4;
+        int year100=year%100;
+        int year400=year%400;
+        if (year4==0){
+            if (year100==0) {
+                if (year400==0) {
+                    System.out.println(year+" год является високосным");
+                } else {
+                    System.out.println(year+" год не является високосным");
+                }
+            } else {
+                System.out.println(year+" год является високосным");
+            }
+        }else {
+            System.out.println(year+" год не является високосным");
+        }
+    }
+    public static void task4() {
+        System.out.println("Задание 4");
+        int deliveryDistance=95;
+        int deliveryDay=1;
+        if (deliveryDistance<=20) {
+            System.out.println("Потребуется дней: "+deliveryDay);
+        } else if (deliveryDistance>20 && deliveryDistance<=60) {
+            System.out.println("Потребуется дней: "+(deliveryDay+1));
+        } else if (deliveryDistance>60 && deliveryDistance<=100) {
+            System.out.println("Потребуется дней: "+(deliveryDay+2));
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+    public static void task5() {
+        System.out.println("Задание 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца нет");
+        }
     }
 }
